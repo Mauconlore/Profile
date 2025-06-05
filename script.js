96691e4cdbfc48e5f796cf9af95f212d3c571f43
Gabriel Faria <106851093+Mauconlore@users.noreply.github.com>
2025-06-05 14:42:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+function mostrarMensagem() {
+    const mensagem = document.getElementById('mensagem');
+    mensagem.innerText = "Obrigado por visitar meu portfólio! 😊";
+    mensagem.style.marginTop = "15px";
+  }
+  
